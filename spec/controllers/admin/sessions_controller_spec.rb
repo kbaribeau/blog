3740938e96e03974d96d958ad8a56145c3aa1dcb
kbaribeau <kevin.baribeau@gmail.com>
2011-03-19 20:32:28 -0500

    fix login with google account.  remove support for any other openid providers

--- spec/controllers/admin/sessions_controller_spec.rb
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -1,14 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::SessionsController do
-  describe 'handling GET to show (default)' do
-    it 'redirects to new' do
-      get :show
-      response.should be_redirect
-      response.should redirect_to(new_admin_session_path)
-    end
-  end
-
   describe 'handling GET to new' do
     before(:each) do
       get :new
@@ -37,13 +29,6 @@ describe Admin::SessionsController do
       response.should redirect_to('/')
     end
   end
-
-  describe '#allow_login_bypass? when RAILS_ENV == production' do
-    it 'returns false' do
-      ::Rails.stub!(:env).and_return('production')
-      @controller.send(:allow_login_bypass?).should == false
-    end
-  end
 end
 
 shared_examples_for "logged in and redirected to /admin" do
@@ -59,12 +44,19 @@ shared_examples_for "not logged in" do
   it "should not set session[:logged_in]" do
     session[:logged_in].should be_nil
   end
-  it "should render new" do
-    response.should be_success
-    response.should render_template("new")
+  it "should render redirect to google auth" do
+    response.should be_redirect
   end
-  it "should set flash.now[:error]" do
-    flash.now[:error].should_not be_nil
+end
+
+shared_examples_for "failed login" do
+
+  it "should not set session[:logged_in]" do
+    session[:logged_in].should be_nil
+  end
+
+  it "should render the new action" do
+    response.should render_template('new')
   end
 end
 
@@ -75,20 +67,17 @@ describe Admin::SessionsController, "handling CREATE with post" do
 
   def stub_open_id_authenticate(url, status_code, return_value)
     status = mock("Result", :successful? => status_code == :successful, :message => '')
-    @controller.stub!(:enki_config).and_return(mock("enki_config", :author_open_ids => [
-        "http://enkiblog.com",
-        "http://secondaryopenid.com"
-      ].collect {|uri| URI.parse(uri)}
-    ))
+    @controller.stub!(:enki_config).and_return(
+      mock("enki_config", 
+        :author_open_ids => [
+          "http://enkiblog.com",
+          "http://secondaryopenid.com"
+        ].map {|uri| URI.parse(uri)}
+      )
+    )
     @controller.should_receive(:authenticate_with_open_id).with(url).and_yield(status,url).and_return(return_value)
   end
-  describe "with invalid URL http://evilman.com and OpenID authentication succeeding" do
-    before do
-      stub_open_id_authenticate("http://evilman.com", :successful, false)
-      post :create, :openid_url => "http://evilman.com"
-    end
-    it_should_behave_like "not logged in"
-  end
+
   describe "with valid URL http://enkiblog.com and OpenID authentication succeeding" do
     before do
       stub_open_id_authenticate("http://enkiblog.com", :successful, false)
@@ -96,51 +85,29 @@ describe Admin::SessionsController, "handling CREATE with post" do
     end
     it_should_behave_like "logged in and redirected to /admin"
   end
-  describe "with valid secondary URL http://secondaryopenid.com and OpenID authentication succeeding" do
-    before do
-      stub_open_id_authenticate("http://secondaryopenid.com", :successful, false)
-      post :create, :openid_url => "http://secondaryopenid.com"
-    end
-    it_should_behave_like "logged in and redirected to /admin"
-  end
+
   describe "with valid URL http://enkiblog.com and OpenID authentication returning 'failed'" do
     before do
       stub_open_id_authenticate("http://enkiblog.com", :failed, true)
       post :create, :openid_url => "http://enkiblog.com"
     end
-    it_should_behave_like "not logged in"
+    it_should_behave_like "failed login"
   end
+
   describe "with valid URL http://enkiblog.com and OpenID authentication returning 'missing'" do
     before do
       stub_open_id_authenticate("http://enkiblog.com", :missing, true)
       post :create, :openid_url => "http://enkiblog.com"
     end
-    it_should_behave_like "not logged in"
+    it_should_behave_like "failed login"
   end
+  
   describe "with valid URL http://enkiblog.com and OpenID authentication returning 'canceled'" do
     before do
       stub_open_id_authenticate("http://enkiblog.com", :canceled, true)
       post :create, :openid_url => "http://enkiblog.com"
     end
-    it_should_behave_like "not logged in"
+    it_should_behave_like "failed login"
   end
-  describe "with no URL" do
-    before do
-      post :create, :openid_url => ""
-    end
-    it_should_behave_like "not logged in"
-  end
-  describe "with bypass login selected" do
-    before do
-      post :create, :openid_url => "", :bypass_login => "1"
-    end
-    it_should_behave_like "logged in and redirected to /admin"
-  end
-  describe "with bypass login selected but login bypassing disabled" do
-    before do
-      @controller.stub!(:allow_login_bypass?).and_return(false)
-      post :create, :openid_url => "", :bypass_login => "1"
-    end
-    it_should_behave_like "not logged in"
-  end
+
 end
